53ebe4cf1293f073099784dc5acc33a7aee1433c
Sajjad Gul <sajjadarifgul@gmail.com>
2019-05-04 16:36:17 +0500

    Installed Entity framework and enabled migrations

---
+++ HMS.Data/HMSContext.cs
@@ -0,0 +1,22 @@
+﻿using HMS.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMS.Data
+{
+    public class HMSContext : DbContext
+    {
+        public HMSContext() : base("HMSConnectionString")
+        {
+        }
+
+        public DbSet<AccomodationType> AccomodationTypes { get; set; }
+        public DbSet<AccomodationPackage> AccomodationPackages { get; set; }
+        public DbSet<Accomodation> Accomodations { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+    }
+}
---
+++ HMS.Data/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace HMS.Data.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<HMS.Data.HMSContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(HMS.Data.HMSContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
