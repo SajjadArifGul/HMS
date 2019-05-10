2502a8f1f0913ff307d7b37b636d45717424135e
Sajjad Gul <sajjadarifgul@gmail.com>
2019-05-10 16:46:59 +0500

    Dashboard Panel for Admins

---
+++ HMS/Areas/Dashboard/Controllers/DashboardController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HMS.Areas.Dashboard.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ HMS/Areas/Dashboard/DashboardAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace HMS.Areas.Dashboard
+{
+    public class DashboardAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Dashboard";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Dashboard_default",
+                "Dashboard/{controller}/{action}/{id}",
+                new { controller = "Dashboard", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
